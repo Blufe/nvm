049883b4da9a014347919a0ff13d600c4b9cd2f5
Greg North <greg.north@hasbro.com>
2014-05-28 14:52:33 -0400

    fixed ruby syntax issue

--- providers/install.rb
+++ providers/install.rb
@@ -8,7 +8,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#		 http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,26 +16,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 def whyrun_supported?
   true
 end
 
 action :create do
-	from_source_message = new_resource.from_source ? ' from source' : ''
-	from_source_arg = new_resource.from_source ? '-s' : ''
+	from_source_message = @new_resource.from_source ? ' from source' : ''
+	from_source_arg = @new_resource.from_source ? '-s' : ''
 	
-	bash "Installing node.js #{new_resource.version}#{from_source_message}..." do
+	bash "Installing node.js #{@new_resource.version}#{from_source_message}..." do
 		code <<-EOH
 			source #{node['nvm']['source']}
-			nvm install #{from_source_arg} #{new_resource.version}
+			nvm install #{from_source_arg} #{@new_resource.version}
 		EOH
 	end
 	
-  if new_resource.alias_as_default
-  	nvm_alias_default new_resource.version do
-  		action :create
-  end
+	if @new_resource.alias_as_default
+		nvm_alias_default @new_resource.version do
+			action :create
+			only_if { @new_resource.alias_as_default }
+		end
+	end
 	
-	new_resource.updated_by_last_action(true)
+	@new_resource.updated_by_last_action(true)
 end
