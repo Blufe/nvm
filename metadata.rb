747188ae369ebfd6ed140ebcb8c35e363eea7227
Blufe <bluephoenixlab@gmail.com>
2014-07-18 13:07:30 +0900

    update version

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,13 @@
-name			 "nvm"
+name		 "nvm"
 maintainer       "Greg North"
 maintainer_email "gregnorth@gmail.com"
 license          "Apache 2.0"
 description      "Installs nvm, the node version manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-supports 		 'ubuntu', ">= 12.04"
-version          "0.1.0"
+version          "0.1.3"
+
+supports "ubuntu", ">= 12.04"
+supports "centos", ">= 6.4"
 
 depends "build-essential"
 depends "git"
